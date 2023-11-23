0c05571dbc5aaad761773a1047d6f07aa28790be
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-11-23 18:04:08 -0500

    tv show ids as python array

--- scripts/scrape_data.py
+++ scripts/scrape_data.py
@@ -34,14 +34,15 @@ def start():
     ratings_df = read_table(ratings_file_path)
 
     tv_series_df = basics_df[basics_df['titleType'] == 'tvSeries']
-
     tv_with_ratings = concat([tv_series_df.set_index('tconst'), ratings_df.set_index('tconst')],  axis=1, join='inner')
+    tv_with_enough_votes = tv_with_ratings[(tv_with_ratings["numVotes"] > 700) & (tv_with_ratings["primaryTitle"].notnull())]
 
-    tv_with_enough_votes = tv_with_ratings[(tv_with_ratings["numVotes"] > 500) & (tv_with_ratings["primaryTitle"].notnull())]
+    sorted_tv_shows = tv_with_enough_votes.sort_values("numVotes", ascending=False)
 
-    print(len(tv_with_enough_votes))
-    # print(tv_with_enough_votes.sort_values("numVotes", ascending=False).head())
+    ids = sorted_tv_shows.index.array.tolist()
 
+    # https://docs.scrapy.org/en/latest/topics/practices.html
+    # https://stackoverflow.com/a/15618520/4198382
     # For every show:
         # Collect every review { user_id, show_id, rating }
     # https://www.imdb.com/title/tt0944947/reviews/
